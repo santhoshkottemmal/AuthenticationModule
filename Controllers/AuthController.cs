cf6f3d1de1198d96af1557babeb36f68b044e282
santhoshkottemmal <114281123+santhoshkottemmal@users.noreply.github.com>
2022-09-24 18:36:57 +0530

    commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationModule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login login)
+        {
+            IActionResult response = Unauthorized();
+
+            var user = await AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+
+                response = Ok(new LoginResponse { token = tokenString});
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(Login login)
+        {
+
+            if (login is null)
+            {
+                throw new ArgumentNullException(nameof(login));
+            }
+            List<Claim> claims = new List<Claim>();
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            claims.Add(new Claim("Username", login.Username));
+            claims.Add(new Claim("role", "admin"));
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(2),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private async Task<Login> AuthenticateUser(Login login)
+        {
+
+            if (login.Username == "admin" && login.Password == "admin")
+            {
+                return login;
+            }
+            else 
+            {
+                throw new Exception("UnAuthenticated");
+            }
+
+            
+        }
+
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationModule
+{
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class LoginResponse 
+    {
+        public string token { get; set; }
+    }
+}
